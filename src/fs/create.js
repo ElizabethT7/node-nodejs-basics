bee2fddc2ae1949ea98f18a06b821166fe096ddb
ElizabethT7 <tymofieievai@gmail.com>
2024-10-06 15:26:12 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+const fileName = 'fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        if (!fs.existsSync(fileName)) {
+          fs.appendFile(`files/${fileName}`, 'I am fresh and young', function (err) {
+            if (err) throw err;
+
+          })
+        }
+      } catch (err) {
+        throw err;
+      }
 };
 
 await create();
